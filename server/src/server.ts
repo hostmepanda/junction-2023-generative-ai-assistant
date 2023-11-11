f39fea48257d800af10533d88412023c43947f2e
hostmepanda <noah.vite@gmail.com>
2023-11-11 15:44:10 +0200

    Add dumb route to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 
-const APP_LISTEN_PORT = 5000
+const APP_LISTEN_PORT =4000
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(APP_LISTEN_PORT, () => {
   console.log(`App is started on port: ${APP_LISTEN_PORT}`)
